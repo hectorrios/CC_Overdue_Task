7f204eb54a63b8d5110d9b6eeb9a6382a714dba2
Hector Rios <oj.rios@gmail.com>
2013-11-15 13:44:26 +0100

    Add the AddTaskViewController class and associated with a view controller from the storyboard

---
+++ OverdueTaskList/CCAddTaskViewController.m
@@ -0,0 +1,38 @@
+//
+//  CCAddTaskViewController.m
+//  OverdueTaskList
+//
+//  Created by Hector Rios on 11/15/13.
+//  Copyright (c) 2013 Hector Rios. All rights reserved.
+//
+
+#import "CCAddTaskViewController.h"
+
+@interface CCAddTaskViewController ()
+
+@end
+
+@implementation CCAddTaskViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
